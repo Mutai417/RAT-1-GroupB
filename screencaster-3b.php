3142fa28b2ff70b3b4696292844a9a5773ee250b
Mutai417 <38202338+Mutai417@users.noreply.github.com>
2019-05-30 16:40:50 +0300

    Add files via upload

---
+++ screencaster-3b.php
@@ -0,0 +1,19 @@
+
+<?php
+
+function convert($value) {
+
+	$result = preg_replace("/[^a-zA-Z]/", " ", $value);
+	return $result;
+}
+
+echo convert("Engineer-Wairuri-Kamau|20.00,15,16");
+echo convert("Doctor Wamvua Ka12mbua `12040102010");
+echo convert("Miss#t0opoi#susan3311");
+echo convert("Engineer-Wairuri-Kamau|20.00,15,16");
+
+
+?>
+
+
+
